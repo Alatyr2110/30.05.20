213b4fb50fab2083683718920d97d042cf890c52
Yana <64301655+Alatyr2110@users.noreply.github.com>
2020-05-30 15:43:28 +0300

    Add files via upload

---
+++ BelieveOrNotBelieve/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace BelieveOrNotBelieve
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.miFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.miNew = new System.Windows.Forms.ToolStripMenuItem();
+            this.miOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.miSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.miExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.tboxQuestion = new System.Windows.Forms.TextBox();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.nudNumber = new System.Windows.Forms.NumericUpDown();
+            this.btnSaveQues = new System.Windows.Forms.Button();
+            this.cboxTrue = new System.Windows.Forms.CheckBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumber)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.miFile});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(10, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(1000, 30);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // miFile
+            // 
+            this.miFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.miNew,
+            this.miOpen,
+            this.miSave,
+            this.toolStripSeparator1,
+            this.miExit});
+            this.miFile.Name = "miFile";
+            this.miFile.Size = new System.Drawing.Size(46, 26);
+            this.miFile.Text = "File";
+            this.miFile.Click += new System.EventHandler(this.miFile_Click);
+            // 
+            // miNew
+            // 
+            this.miNew.Name = "miNew";
+            this.miNew.Size = new System.Drawing.Size(128, 26);
+            this.miNew.Text = "New";
+            this.miNew.Click += new System.EventHandler(this.miNew_Click);
+            // 
+            // miOpen
+            // 
+            this.miOpen.Name = "miOpen";
+            this.miOpen.Size = new System.Drawing.Size(128, 26);
+            this.miOpen.Text = "Open";
+            this.miOpen.Click += new System.EventHandler(this.miOpen_Click);
+            // 
+            // miSave
+            // 
+            this.miSave.Name = "miSave";
+            this.miSave.Size = new System.Drawing.Size(128, 26);
+            this.miSave.Text = "Save";
+            this.miSave.Click += new System.EventHandler(this.miSave_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(125, 6);
+            // 
+            // miExit
+            // 
+            this.miExit.Name = "miExit";
+            this.miExit.Size = new System.Drawing.Size(128, 26);
+            this.miExit.Text = "Exit";
+            this.miExit.Click += new System.EventHandler(this.miExit_Click);
+            // 
+            // tboxQuestion
+            // 
+            this.tboxQuestion.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tboxQuestion.Location = new System.Drawing.Point(0, 30);
+            this.tboxQuestion.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tboxQuestion.Multiline = true;
+            this.tboxQuestion.Name = "tboxQuestion";
+            this.tboxQuestion.Size = new System.Drawing.Size(1000, 228);
+            this.tboxQuestion.TabIndex = 1;
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(16, 296);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(122, 42);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "Добавить";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(204, 296);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(125, 42);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "Удалить";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // nudNumber
+            // 
+            this.nudNumber.Location = new System.Drawing.Point(585, 295);
+            this.nudNumber.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.nudNumber.Name = "nudNumber";
+            this.nudNumber.Size = new System.Drawing.Size(150, 27);
+            this.nudNumber.TabIndex = 5;
+            this.nudNumber.ValueChanged += new System.EventHandler(this.nudNumber_ValueChanged);
+            // 
+            // btnSaveQues
+            // 
+            this.btnSaveQues.Location = new System.Drawing.Point(389, 295);
+            this.btnSaveQues.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSaveQues.Name = "btnSaveQues";
+            this.btnSaveQues.Size = new System.Drawing.Size(125, 42);
+            this.btnSaveQues.TabIndex = 6;
+            this.btnSaveQues.Text = "Сохранить";
+            this.btnSaveQues.UseVisualStyleBackColor = true;
+            this.btnSaveQues.Click += new System.EventHandler(this.btnSaveQues_Click);
+            // 
+            // cboxTrue
+            // 
+            this.cboxTrue.AutoSize = true;
+            this.cboxTrue.Location = new System.Drawing.Point(780, 296);
+            this.cboxTrue.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cboxTrue.Name = "cboxTrue";
+            this.cboxTrue.Size = new System.Drawing.Size(84, 24);
+            this.cboxTrue.TabIndex = 7;
+            this.cboxTrue.Text = "Правда";
+            this.cboxTrue.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(1000, 562);
+            this.Controls.Add(this.cboxTrue);
+            this.Controls.Add(this.btnSaveQues);
+            this.Controls.Add(this.nudNumber);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.tboxQuestion);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Monotype Corsiva", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "Верю - Не верю";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumber)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem miFile;
+        private System.Windows.Forms.ToolStripMenuItem miNew;
+        private System.Windows.Forms.ToolStripMenuItem miOpen;
+        private System.Windows.Forms.ToolStripMenuItem miSave;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem miExit;
+        private System.Windows.Forms.TextBox tboxQuestion;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.NumericUpDown nudNumber;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSaveQues;
+        private System.Windows.Forms.CheckBox cboxTrue;
+    }
+}
+
